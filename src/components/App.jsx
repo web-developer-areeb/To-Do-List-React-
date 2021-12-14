9d3331d0d30c025d4eac94f54dea8dfca844e1a9
Areeb <areeb.ahmad.me@gmail.com>
2021-12-14 13:17:14 +0530

    UI is designed for To do item and header.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
+import '../css/App.css';
 
 import Todos from './todos';
 
 class App extends React.Component {
+    state = {
+        todos: [
+            {
+                id: 1,
+                title: 'First todo',
+                description: 'This is the description of first todo. Do you like it or not. It looks preety awesome.'
+            },
+            {
+                id: 2,
+                title: 'Second todo',
+                description: 'This is the description of first todo. Do you like it or not. It looks preety awesome.'
+            }
+        ]
+    }
+
     render() {
         return (
-            <div>
-                <h1 >To Do List</h1>
-                <Todos />
+            <div className='container'>
+                <div className="header">
+                    <h1 >To Do List</h1>
+                </div>
+                <Todos todoItems={this.state.todos} />
             </div>
         );
     };
--- src/components/todos.jsx
+++ src/components/todos.jsx
@@ -1,14 +1,34 @@
 import React from "react";
+import '../css/Todos.css';
 
 class Todos extends React.Component {
+
+    renderList(todos) {
+        return todos.map(todo => {
+            return (
+                <div key={todo.id} className="todo__container" >
+                    <div className="todo__title">
+                        {todo.title}
+                    </div>
+                    <div className="todo__description">
+                        {todo.description}
+                    </div>
+                    <div className="todo__action">
+                        <button className="btn btn-edit">Edit</button>
+                        <button className="btn btn-delete">Delete</button>
+                    </div>
+                </div>
+            );
+        })
+    }
+
     render() {
         return (
             <div>
-                Todos
+                {this.renderList(this.props.todoItems)}
             </div>
         );
-
-    };
-};
+    }
+}
 
 export default Todos;
